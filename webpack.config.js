48bd1f7b05fb97eff40f89260473761c2a697b96
Félix Girault <felix.girault@gmail.com>
2015-03-27 22:06:30 +0100

    Updated build config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@
  */
 'use strict';
 
+var webpack = require('webpack');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 
 
 /**
@@ -16,21 +20,49 @@ module.exports = {
 		library: 'Reaccess',
 		libraryTarget: 'umd',
 		path: './dist',
+		publicPath: '/demo/',
 		filename: 'reaccess.js'
 	},
-	externals: [{
-		'react': {
-			root: 'React',
-			commonjs: 'react',
-			commonjs2: 'react',
-			amd: 'react'
+	externals: [
+		{
+			'react': {
+				root: 'React',
+				commonjs: 'react',
+				commonjs2: 'react',
+				amd: 'react'
+			}
 		}
-	}],
+	],
+	eslint: {
+		reporter: require('eslint-friendly-formatter')
+	},
 	module: {
-		loaders: [{
-			loader: 'babel-loader?experimental&optional=runtime',
-			test: /\.js$/,
-			exclude: /node_modules/
-		}]
-	}
+		preLoaders: [
+			{
+				loader: 'eslint-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			}
+		],
+		loaders: [
+			{
+				loader: 'babel-loader?experimental&optional=runtime',
+				test: /\.js$/,
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract(
+					'style-loader',
+					'css-loader'
+				)
+			}
+		]
+	},
+	plugins: [
+		new UglifyJsPlugin(),
+		new ExtractTextPlugin('reaccess.css', {
+			allChunks: true
+        })
+	]
 }
