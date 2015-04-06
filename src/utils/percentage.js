bf2356c49f8a092f263824cc3bdcbb76d7d2a0c3
Félix Girault <felix.girault@gmail.com>
2015-04-06 18:02:56 +0200

    Fixed percentage calculation

--- src/utils/percentage.js
+++ src/utils/percentage.js
@@ -9,7 +9,13 @@
  *
  */
 export default function percentage(value, max, min = 0) {
-	return value
-		? ((value * 100) / (max - min))
-		: 0;
+	if (value <= min) {
+		return 0;
+	}
+
+	if (value >= max) {
+		return 100;
+	}
+
+	return ((value - min) * 100) / (max - min);
 }
