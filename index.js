c6b533f1389589cd2518d3ecddf7f3a245a1724b
Félix Girault <felix.girault@gmail.com>
2015-09-15 01:25:07 +0200

    Using the re-export syntax

--- index.js
+++ index.js
@@ -3,37 +3,16 @@
  */
 'use strict';
 
-import Button from './src/components/Button';
-import ToggleButton from './src/components/ToggleButton';
-import StatefulToggleButton from './src/components/StatefulToggleButton';
-import NumberInput from './src/components/NumberInput';
-import StatefulNumberInput from './src/components/StatefulNumberInput';
-import ProgressBar from './src/components/ProgressBar';
-import Slider from './src/components/Slider';
-import StatefulSlider from './src/components/StatefulSlider';
-import RangeSlider from './src/components/RangeSlider';
-import StatefulRangeSlider from './src/components/StatefulRangeSlider';
-import Tabs from './src/components/Tabs';
-import StatefulTabs from './src/components/StatefulTabs';
-import Panel from './src/components/Panel';
-
-
-
-/**
- *
- */
-export default {
-	Button,
-	ToggleButton,
-	StatefulToggleButton,
-	NumberInput,
-	StatefulNumberInput,
-	ProgressBar,
-	Slider,
-	StatefulSlider,
-	RangeSlider,
-	StatefulRangeSlider,
-	Tabs,
-	StatefulTabs,
-	Panel
-};
+export {default as Button} from './src/components/Button';
+export {default as NumberInput} from './src/components/NumberInput';
+export {default as Panel} from './src/components/Panel';
+export {default as ProgressBar} from './src/components/ProgressBar';
+export {default as RangeSlider} from './src/components/RangeSlider';
+export {default as Slider} from './src/components/Slider';
+export {default as StatefulNumberInput} from './src/components/StatefulNumberInput';
+export {default as StatefulRangeSlider} from './src/components/StatefulRangeSlider';
+export {default as StatefulSlider} from './src/components/StatefulSlider';
+export {default as StatefulTabs} from './src/components/StatefulTabs';
+export {default as StatefulToggleButton} from './src/components/StatefulToggleButton';
+export {default as Tabs} from './src/components/Tabs';
+export {default as ToggleButton} from './src/components/ToggleButton';
