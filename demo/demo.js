02fe618e47d4df309f212a2188736e376186b978
Félix Girault <felix.girault@gmail.com>
2015-09-17 02:08:37 +0200

    Added a demo of the Modal component

--- demo/demo.js
+++ demo/demo.js
@@ -145,4 +145,86 @@
 		/>,
 		$('toggle-button')
 	);
+
+
+
+	/**
+	 *
+	 */
+	var StatefulModal = React.createClass({
+
+		getInitialState: function() {
+			return {
+				opened: false
+			};
+		},
+
+		handleOpen: function() {
+			this.setState({
+				opened: true
+			})
+		},
+
+		handleClose: function() {
+			this.setState({
+				opened: false
+			})
+		},
+
+		render: function() {
+			return (
+				<div>
+					<button onClick={this.handleOpen}>Open</button>
+					{this.modal()}
+				</div>
+			);
+		},
+
+		modal: function() {
+			if (!this.state.opened) {
+				return undefined;
+			}
+
+			return (
+				<Rea11y.Modal title="Modal" labelId="modal-title">
+					<div className="rea11y-modal-header">
+						<h1 className="rea11y-modal-title">
+							This is a modal
+						</h1>
+					</div>
+
+					<div className="rea11y-modal-body">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua.
+						</p>
+
+						<p>
+							Ut enim ad minim veniam, quis nostrud exercitation
+							ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in
+							voluptate velit esse cillum dolore eu fugiat nulla
+							pariatur.
+						</p>
+
+						<p>
+							Excepteur sint occaecat cupidatat non proident, sunt
+							in culpa qui officia deserunt mollit anim id est
+							laborum.
+						</p>
+					</div>
+
+					<div className="rea11y-modal-footer">
+						<button>Do nothing</button>
+						<button onClick={this.handleClose}>
+							Close
+						</button>
+					</div>
+				</Rea11y.Modal>
+			);
+		}
+	});
+
+	React.render(<StatefulModal />, $('modal'));
 })();
