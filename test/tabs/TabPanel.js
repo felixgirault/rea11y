88527fc4ba473471400e31d0f1425e3586f78f59
Félix Girault <felix.girault@gmail.com>
2016-02-25 21:37:10 +0100

    Added tests for Tab and TabPanel

---
+++ test/tabs/TabPanel.js
@@ -0,0 +1,60 @@
+/**
+ *
+ */
+import jsdom from 'jsdom-global';
+import {expect} from 'chai';
+import React from 'react';
+import {findDOMNode} from 'react-dom';
+import {renderIntoDocument} from 'react-addons-test-utils';
+import TabPanel from '../../src/tabs/TabPanel';
+
+
+
+/**
+ *
+ */
+describe('TabPanel', function() {
+
+	before(function() {
+		this.cleanup = jsdom();
+	});
+
+	after(function() {
+		this.cleanup();
+	});
+
+	/**
+	 *
+	 */
+	it('should have appropriate attributes', function() {
+		const panel = renderIntoDocument(
+			<div>
+				<TabPanel id="id" name="name" />
+			</div>
+		);
+
+		const node = findDOMNode(panel).children[0];
+
+		expect(node.getAttribute('id')).to.equal('id-panel-name');
+		expect(node.getAttribute('role')).to.equal('tabpanel');
+		expect(node.getAttribute('aria-hidden')).to.equal('true');
+		expect(node.getAttribute('aria-labelledby')).to.equal('id-name');
+		expect(node.getAttribute('tabindex')).to.equal('-1');
+	});
+
+	/**
+	 *
+	 */
+	it('should have appropriate attributes when active', function() {
+		const panel = renderIntoDocument(
+			<div>
+				<TabPanel id="id" name="name" active />
+			</div>
+		);
+
+		const node = findDOMNode(panel).children[0];
+
+		expect(node.getAttribute('aria-hidden')).to.equal('false');
+		expect(node.getAttribute('tabindex')).to.equal('0');
+	});
+});
