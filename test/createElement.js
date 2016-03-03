5f61ca1ed023e6aad6ff4c1e7762e6a7f15b6027
Félix Girault <felix.girault@gmail.com>
2016-03-04 00:40:46 +0100

    Added tests for tabbable

---
+++ test/createElement.js
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+import {forEach} from 'lodash';
+
+
+
+/**
+ *
+ */
+export default function createElement(name, attributes = {}) {
+	const element = document.createElement(name);
+
+	forEach(attributes, (value, key) =>
+		element.setAttribute(key, value)
+	);
+
+	return element;
+}
